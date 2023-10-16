7f2233e18bc0c0b3550a308a4fb8e98aa068a173
Saba Homayounibaghbidi <homayounibaghbidi.saba@gmail.com>
2023-10-16 20:50:20 +0200

    cache signals data to find an alternative solution for subscription

--- app/src/main/java/com/plcoding/graphqlmobileapp/domain/GetSignalUseCase.kt
+++ app/src/main/java/com/plcoding/graphqlmobileapp/domain/GetSignalUseCase.kt
@@ -1,9 +1,45 @@
 package com.plcoding.graphqlmobileapp.domain
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.plcoding.SignalsByFilterQuery
+import java.time.LocalDateTime
+
 class GetSignalUseCase(
     private val signalClient: SignalClient
 ) {
+
+    //TODO need cleanup, add time to filter
+    val signalsMap = mutableMapOf<String, SignalCache>()
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun execute(id: String): List<DetailedSignalData>? {
+        if (signalsMap[id] == null) {
+            println("*****1*****")
+            val lst = mutableListOf<DetailedSignalData>()
+            fetchAndUpdate(id)?.let { lst.addAll(it) }
+            signalsMap[id] = SignalCache(LocalDateTime.now(), lst)
+        } else {
+            println("*****2*****")
+            val signalCache = signalsMap[id]
+            if (signalCache!!.lastFetch.isBefore(LocalDateTime.now().minusMinutes(10))) {
+                val lst = signalCache!!.signals
+                fetchAndUpdate(id)?.let { lst.addAll(it) }
+                signalsMap[id] = SignalCache(
+                    LocalDateTime.now(),
+                    signals = lst
+                )
+            }
+        }
+        return signalsMap[id]!!.signals
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private suspend fun fetchAndUpdate(id: String): List<DetailedSignalData>? {
         return signalClient.getSignals(id)
     }
 }
+
+data class SignalCache(
+    val lastFetch: LocalDateTime,
+    val signals: MutableList<DetailedSignalData>
+)
---
+++ app/src/main/java/com/plcoding/graphqlmobileapp/utils/Helper.kt
@@ -0,0 +1,23 @@
+package com.plcoding.graphqlmobileapp.utils
+
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
+
+
+object Helper {
+    private const val TAG = "Helper"
+    fun getConfigValue(name: String): String? {
+////    fun main() {
+//        val file = File("/res/config.properties")
+//
+//        val prop = Properties()
+//        FileInputStream(file).use { prop.load(it) }
+//
+//        // Print all properties
+//        val a = prop.stringPropertyNames()
+//            .associateWith {prop.getProperty(it)}
+//            .firstOrNull { println(it) }
+        return null
+    }
+}
